2a1a0331bbdb4e92b44247a402f9feff510729de
Nithin Bhat <Nithin.Bhat@triconinfotech.com>
2024-04-05 12:30:45 +0530

    did

--- app/main_test.py
+++ app/main_test.py
@@ -3,7 +3,7 @@ import unittest
 
 class TestMain(unittest.TestCase):
     def test_return_backwards_string(self):
-        random_string = "kannada is malayanlam "
+        random_string = "kannada is malayalam"
         expected_result = "malayalam si adannak"  
         self.assertEqual(expected_result, return_backwards_string(random_string)) 
 
